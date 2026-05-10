c92937e7d7cd583b43f89634e37b3acc2936019d
Dennis Dole <djdole@gmail.com>
2026-05-10 14:23:29 -0700

    updates

--- wipeJenkins.sh
+++ wipeJenkins.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo rm -rf /var/lib/docker/volumes/jenkins_jenkins_data/_data/workspace/agent-smith
-sudo rm -rf /var/lib/docker/volumes/jenkins_jenkins_data/_data/workspace/agent-smith@tmp
+sudo rm -rf /var/lib/docker/volumes/jenkins_jenkins_data/_data/workspace/the-architect
+sudo rm -rf /var/lib/docker/volumes/jenkins_jenkins_data/_data/workspace/the-architect*
+sudo rm -rf /var/lib/docker/volumes/jenkins_jenkins_data/_data/workspace/the-architect@tmp
